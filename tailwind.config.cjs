5a10daf2015fa591f0f9f9b4ad75095d3e01923c
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2023-01-08 16:29:39 -0700

    font and typography stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      heading: ['Overpass', 'sans-serif'],
-      body: ['Overpass', 'sans-serif'],
-      mono: ['Overpass Mono', 'monospace'],
+      heading: ['Rubik', 'sans-serif'],
+      body: ['Rubik', 'sans-serif'],
+      mono: ['monospace'],
     },
     extend: {},
   },
