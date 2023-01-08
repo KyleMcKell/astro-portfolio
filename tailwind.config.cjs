07e155a40eb68316b1774eff747c82e9441f8a7a
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2023-01-08 15:07:56 -0700

    changing fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      heading: ['Sora', 'sans-serif'],
-      body: ['Sora', 'sans-serif'],
-      mono: ['monospace'],
+      heading: ['Overpass', 'sans-serif'],
+      body: ['Overpass', 'sans-serif'],
+      mono: ['Overpass Mono', 'monospace'],
     },
     extend: {},
   },
